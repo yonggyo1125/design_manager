02b76f88e0e8633ab1c1da651ce8865e7bb095ce
yonggyo1125 <yonggyo00@kakao.com>
2022-11-30 16:38:44 +0900

    SSL 적용 처리

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 const app = require('./app');
-
-app.listen(app.get('PORT'), () => {
-    console.log("markers design Manager Listening : PORT : ", app.get('PORT'));
-});
+const env = process.env.NODE_ENV || "development";
+if (env == 'production') {
+    const options = {
+        key: fs.readFileSync(path.join(__dirname, 'keys/private.key')),
+        cert: fs.readFileSync(path.join(__dirname, 'keys/certificate.crt')),
+        ca: fs.readFileSync(path.join(__dirname, 'keys/ca_bundle.crt')),
+    };
+    const server = https.createServer(options, app);
+    server.listen(app.get('PORT'), () => {
+        console.log("markers design Manager Listening : PORT : ", app.get('PORT'));
+    });
+} else {
+    app.listen(app.get('PORT'), () => {
+        console.log("markers design Manager Listening : PORT : ", app.get('PORT'));
+    });
+}
