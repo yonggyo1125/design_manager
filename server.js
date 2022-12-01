0cf9268a5ea7caf0b7d822e843513a791c41b29d
yonggyo1125 <yonggyo00@kakao.com>
2022-12-01 10:06:29 +0900

    https 내용 반영

--- server.js
+++ server.js
@@ -1,20 +1,5 @@
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
 const app = require('./app');
-const env = process.env.NODE_ENV || "development";
-if (env == 'production') {
-    const options = {
-        key: fs.readFileSync(path.join(__dirname, 'keys/private.key')),
-        cert: fs.readFileSync(path.join(__dirname, 'keys/certificate.crt')),
-        ca: fs.readFileSync(path.join(__dirname, 'keys/ca_bundle.crt')),
-    };
-    const server = https.createServer(options, app);
-    server.listen(app.get('PORT'), () => {
-        console.log("markers design Manager Listening : PORT : ", app.get('PORT'));
-    });
-} else {
-    app.listen(app.get('PORT'), () => {
-        console.log("markers design Manager Listening : PORT : ", app.get('PORT'));
-    });
-}
+
+app.listen(app.get('PORT'), () => {
+    console.log("markers design Manager Listening : PORT : ", app.get('PORT'));
+});
