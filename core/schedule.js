32167335be76be264757285195b61830faeacf47
yonggyo1125 <yonggyo00@kakao.com>
2022-12-24 10:31:28 +0900

    오류 수정

--- core/schedule.js
+++ core/schedule.js
@@ -6,12 +6,12 @@ const kakaoAlimTalkApi =  require('../service/kakaoAlimTalk/api');
  */
 // 1분에 한번씩 실행
 module.exports = (req, res, next) => {
-    schedule.scheduleJob('0 */5 * * * *', () => {
+    schedule.scheduleJob('0 */10 * * * *', () => {
       // 카카오 알림톡 전송 결과 업데이트 
       kakaoAlimTalkApi.updateSendResults();
     });
 
-    schedule.scheduleJob('0 */10 * * * *', () => {
+    schedule.scheduleJob('0 */20 * * * *', () => {
       kakaoAlimTalkApi.sendReserveds();
     });
     
--- server.js
+++ server.js
@@ -3,3 +3,17 @@ const app = require('./app');
 app.listen(app.get('PORT'), () => {
     console.log("markers design Manager Listening : PORT : ", app.get('PORT'));
 });
+
+process.on('uncaughtException', (err) => {
+    console.error("처리하지 못한 오류 - 확인 요망 : ", err.message);
+    console.error(err);
+     process.exit(0);
+});
+
+
+process.on('SIGINT', () => {
+    app.close(() => {
+        console.log('server closed');
+        process.exit(0);
+    });
+});
