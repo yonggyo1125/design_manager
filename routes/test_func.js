c6b807ef1916bcefcd7acc47637c12693d8e8843
yonggyo1125 <yonggyo00@kakao.com>
2022-12-26 08:45:44 +0900

    게시글 이전

--- routes/test_func.js
+++ routes/test_func.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const express = require('express');
-const { sequelize, Sequelize : { Op }} = require("../models");
+const { BoardData } = require("../models");
 require('express-async-errors');
 const router = express.Router();
 
@@ -12,9 +12,32 @@ router.get("/", async (req, res) => {
         url : "https://n-mk.com/shop/dbport/board.php?id=training",
     });
     
-
+    let listOrder = Date.now();
     for (const li of result.data) {
-        console.log(li);
+        listOrder++;
+        const cnt = await BoardData.count({ where : {extra1 : li.idx}});
+        if (cnt > 0) continue;
+        let contents = li.contents;
+        contents = contents.replace(/http:\/\/n-mk.com/g, "https://dm.n-mk.kr");
+        const params = {
+            gid : li.gid,
+            poster : li.post_name,
+            guestPw : li.password,
+            isNotice : li.is_notice == "1"?true:false,
+            category : li.category,
+            subject : li.subject,
+            content : contents,
+            totalComments : li.commentCnt || 0,
+            ipAddr : li.ip,
+            userAgent : "",
+            useEditor : true,
+            extra1 : li.idx,
+            listOrder,
+            idBoard : "training",
+            idManager : 2,
+        };
+
+        await BoardData.create(params); 
     }
 
 
